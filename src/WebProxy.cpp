11ef8f11d6b9438123852474fd7ee4aed28b720c
Talal Alothman <alothman@uga.edu>
2016-03-05 13:58:16 -0500

    Proxy recives HTTP requests

--- src/WebProxy.cpp
+++ src/WebProxy.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
@@ -16,18 +17,32 @@ class tcp_connection: public boost::enable_shared_from_this<tcp_connection>{
 		static pointer create(io_service& io_service){
 			return pointer(new tcp_connection(io_service));
 		}
-	
+		
+		//returns a reference to the socket being used for this
+		//tcp_connection.
 		tcp::socket& socket(){
 			return socket_;
 		}
 		
-		//TODO
+		//TODO to parse and possibly output what's being read
+		//from the client. //TODO Connect to server based on 
+		//what was parsed.
 		void start(){
-			
+			boost::array<char, 1024> buf;
+			boost::system::error_code error;
+
+			size_t len = socket_.read_some(buffer(buf), error);
+
+		//	if(error == error::eof)
+		//		break;
+		//	else if (error)
+		//		throw boost::system::system_error(error);
+
+			cout.write(buf.data(), len);
 		}
 	
 	private:
-		//class contructor
+		//class contructor that assigns the socket_
 		tcp_connection(io_service& io_service) : socket_(io_service){}
 		void handle_write(const boost::system::error_code&, size_t){}
 		
@@ -44,15 +59,22 @@ class tcp_server{
 	private:
 		void start_accept(){
 			tcp_connection::pointer new_connection = tcp_connection::create(acceptor_.get_io_service());
-			//acceptor_.async_accept(new_connection->socket(), boost::bind(&tcp_server::handle_accept, this, new_connection, placeholders::error));
+			
+			//begin accepting clients asyncronously
 			acceptor_.async_accept(new_connection->socket(), boost::bind(&tcp_server::handle_accept, this, new_connection, boost::asio::placeholders::error));
+
 		} 
 		
 		void handle_accept(tcp_connection::pointer new_connection, const boost::system::error_code& error){
+			//if we don't get an error with our accepted tcp connection,
+			//we call start() to get the job done. TODO modify start() 
+			//to parse incoming requests and connect to the right server. 
 			if(!error){
 				new_connection->start();
 			}
 			
+			//start accepting some more. This is the main part of the
+			//async feature.
 			start_accept();
 		} 
 		
